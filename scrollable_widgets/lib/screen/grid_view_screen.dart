9f026383b41eeb6bea42e4b25b281e1fb99d5c38
O2SI <O2SI_@naver.com>
2024-11-06 17:02:47 +0900

    yappi

---
+++ scrollable_widgets/lib/screen/grid_view_screen.dart
@@ -0,0 +1,89 @@
+import 'package:flutter/material.dart';
+import 'package:scrollable_widgets/const/colors.dart';
+import 'package:scrollable_widgets/layout/main_layout.dart';
+
+class GridViewScreen extends StatelessWidget {
+  List<int> numbers = List.generate(100, (index) => index);
+
+  GridViewScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MainLayout(
+      title: 'GridViewScreen',
+      body: renderMaxExtent(),
+    );
+  }
+
+  //1. 한번에 다 그림
+  Widget renderConut() {
+    return GridView.count(
+      crossAxisCount: 2,
+      crossAxisSpacing: 12,
+      mainAxisSpacing: 12,
+      children: numbers
+          .map(
+            (e) => renderContainer(
+              color: raindowColors[e % raindowColors.length],
+              index: e,
+            ),
+          )
+          .toList(),
+    );
+  }
+
+  //2. 보이는것만 그림
+  Widget renderBuilderCrossAxisCount(){
+    return GridView.builder(
+        gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+          crossAxisCount: 2,
+          crossAxisSpacing: 12,
+          mainAxisSpacing: 12,
+        ),
+        itemBuilder: (context, index) {
+          return renderContainer(
+            color: raindowColors[index % raindowColors.length],
+            index: index,
+          );
+        },
+        //itemCount: 100, 
+      );
+  }
+  //3. 최대 사이즈
+  Widget renderMaxExtent(){
+     return GridView.builder(
+        gridDelegate: SliverGridDelegateWithMaxCrossAxisExtent(
+          maxCrossAxisExtent: 220,
+        ),
+        itemBuilder: (context, index) {
+          return renderContainer(
+            color: raindowColors[index % raindowColors.length],
+            index: index,
+          );
+        },
+        itemCount: 100,
+      );
+  }
+
+  Widget renderContainer({
+    required Color color,
+    required int index,
+    double? height,
+  }) {
+    print(index);
+    return Container(
+      height: height ?? 300,
+      color: color,
+      child: Center(
+        child: Text(
+          index.toString(),
+          style: TextStyle(
+            color: Colors.white,
+            fontWeight: FontWeight.w700,
+            fontSize: 30,
+          ),
+        ),
+      ),
+    );
+  }
+}
--- scrollable_widgets/lib/screen/home_screen.dart
+++ scrollable_widgets/lib/screen/home_screen.dart
@@ -1,9 +1,27 @@
 import 'package:flutter/material.dart';
 import 'package:scrollable_widgets/layout/main_layout.dart';
+import 'package:scrollable_widgets/screen/grid_view_screen.dart';
+import 'package:scrollable_widgets/screen/list_view_screen.dart';
+import 'package:scrollable_widgets/screen/reorderable_list_view_screen.dart';
 import 'package:scrollable_widgets/screen/single_child_scroll_view_screen.dart';
 
+class ScreenModel {
+  final WidgetBuilder builder;
+  final String name;
+
+  ScreenModel({required this.builder, required this.name});
+}
+
 class HomeScreen extends StatelessWidget {
-  const HomeScreen({super.key});
+  final screens = [
+    ScreenModel(
+        builder: (_) => SingleChildScrollViewScreen(),
+        name: 'SingleChildScrollViewScreen'),
+    ScreenModel(builder: (_) => ListViewScreen(), name: 'ListViewScreen'),
+    ScreenModel(builder: (_) => GridViewScreen(), name: 'GridViewScreen'),
+    ScreenModel(builder: (_) => ReorderableListViewScreen(), name: 'ReorderableListViewScreen'),
+  ];
+  HomeScreen({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
@@ -11,21 +29,23 @@ class HomeScreen extends StatelessWidget {
       title: 'Home',
       body: Padding(
         padding: const EdgeInsets.symmetric(horizontal: 16),
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          crossAxisAlignment: CrossAxisAlignment.stretch,
-          children: [
-            ElevatedButton(
-              onPressed: () {
-                Navigator.of(context).push(
-                  MaterialPageRoute(
-                    builder: (_) => SingleChildScrollViewScreen(),
+        child: SingleChildScrollView(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            crossAxisAlignment: CrossAxisAlignment.stretch,
+            children: screens
+                .map(
+                  (screen) => ElevatedButton(
+                    onPressed: () {
+                      Navigator.of(context).push(
+                        MaterialPageRoute(builder: screen.builder),
+                      );
+                    },
+                    child: Text(screen.name),
                   ),
-                );
-              },
-              child: Text('SingleChildScrollView'),
-            ),
-          ],
+                )
+                .toList(),
+          ),
         ),
       ),
     );
