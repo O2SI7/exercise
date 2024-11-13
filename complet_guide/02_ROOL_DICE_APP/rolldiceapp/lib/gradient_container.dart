14e2ba51550487ee8a5309e2e47c0d6d55f354c6
jaeyu <jaeyu.kim@superb.today>
2024-11-13 12:25:40 +0900

    2강의-변수활용

---
+++ complet_guide/02_ROOL_DICE_APP/rolldiceapp/lib/gradient_container.dart
@@ -0,0 +1,50 @@
+import 'package:flutter/material.dart';
+import 'package:rolldiceapp/styled_text.dart';
+
+const startAlignment = Alignment.topLeft;
+const endAlignment = Alignment.bottomRight;
+
+class GradientContainer extends StatelessWidget {
+  const GradientContainer(this.col1, this.col2,{super.key});
+
+  final Color col1;
+  final Color col2;
+
+  @override
+  Widget build(context) {
+    return Container(
+      decoration: BoxDecoration(
+        gradient: LinearGradient(
+          colors: [col1,col2],
+          begin: startAlignment,
+          end: endAlignment,
+        ),
+      ),
+      child: const Center(
+        child: StyledText('Hello World!'),
+      ),
+    );
+  }
+}
+
+// class GradientContainer extends StatelessWidget {
+//   const GradientContainer({super.key, required this.colors});
+
+//   final List<Color> colors;
+
+//   @override
+//   Widget build(context) {
+//     return Container(
+//       decoration: BoxDecoration(
+//         gradient: LinearGradient(
+//           colors: colors,
+//           begin: startAlignment,
+//           end: endAlignment,
+//         ),
+//       ),
+//       child: const Center(
+//         child: StyledText('Hello World!'),
+//       ),
+//     );
+//   }
+// }
