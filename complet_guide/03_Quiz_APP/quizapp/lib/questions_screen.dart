b60bb06b8a4090a2caa344ec5855444d3687270f
1x3-4 <154939142+1x3-4@users.noreply.github.com>
2024-11-19 16:11:47 +0900

    Quizapp (#6)
    * initial commit
    
    * 3강 첫스크린만들기
    
    * 3강의 퀴즈앱만들기
    
    * 3강의 퀴즈앱만들기 초반부-버튼
    
    * 오류
    
    * 퀴즈앱만들기 완
    
    * 디버그
    
    ---------
    
    Co-authored-by: jaeyu <jaeyu.kim@superb.today>

---
+++ complet_guide/03_Quiz_APP/quizapp/lib/questions_screen.dart
@@ -0,0 +1,66 @@
+import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+import 'package:quizapp/answer_button.dart';
+import 'package:quizapp/data/questions.dart';
+
+
+class QuestionsScreen extends StatefulWidget {
+  const QuestionsScreen({super.key, required this.onSelectAnswer});
+
+  final void Function(String answer) onSelectAnswer;
+
+
+  @override
+  State<QuestionsScreen> createState() {
+    return _QuestionsScreenState();
+  }
+}
+
+class _QuestionsScreenState extends State<QuestionsScreen> {
+  var currentQuestionIndex = 0;
+
+  void answerQuestion(String selectedAnswers) {
+    widget.onSelectAnswer(selectedAnswers);
+    // currentQuestionIndex = currentQuestionIndex +1;
+    // currentQuestionIndex += 1;
+    setState(() {
+      currentQuestionIndex++;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    final currentQuestion = questions[currentQuestionIndex];
+
+    return SizedBox(
+      width: double.infinity,
+      child: Container(
+        margin: const EdgeInsets.all(40),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          crossAxisAlignment: CrossAxisAlignment.stretch,
+          children: [
+            Text(
+              currentQuestion.text,
+              style: GoogleFonts.lato(
+                color: const Color.fromARGB(255, 187, 114, 255),
+                fontSize: 24,
+                fontWeight: FontWeight.bold,
+              ),
+              textAlign: TextAlign.center,
+            ),
+            const SizedBox(height: 30),
+            ...currentQuestion.shuffledAnswers.map((answer) {
+              return AnswerButton(
+                answerText: answer,
+                onTap: (){
+                  answerQuestion(answer);
+                },
+              );
+            })
+          ],
+        ),
+      ),
+    );
+  }
+}
---
+++ complet_guide/03_Quiz_APP/quizapp/lib/quiz.dart
@@ -0,0 +1,89 @@
+import 'package:flutter/material.dart';
+import 'package:quizapp/data/questions.dart';
+import 'package:quizapp/questions_screen.dart';
+import 'package:quizapp/start_screen.dart';
+import 'package:quizapp/results_screen.dart';
+
+class Quiz extends StatefulWidget {
+  const Quiz({super.key});
+
+  @override
+  State<Quiz> createState() {
+    return _QuizState();
+  }
+}
+
+class _QuizState extends State<Quiz> {
+  List<String> _selectedAnswers = [];
+  var activeScreen = 'start-screen';
+  // Widget? activeScreen;
+
+  // @override
+  // void initState() {
+  //   activeScreen = StartScreen(switchScreen);
+  //   super.initState();
+  // }
+
+  void switchScreen() {
+    setState(() {
+      // activeScreen = QuestionsScreen();
+      activeScreen = 'questions-screen';
+    });
+  }
+
+  void chooseAnswer(String selectedanswer) {
+    _selectedAnswers.add(selectedanswer);
+
+    if (_selectedAnswers.length == questions.length) {
+      setState(() {
+        activeScreen = 'results-screen';
+      });
+    }
+  }
+
+  void restartQuiz() {
+    setState(() {
+      _selectedAnswers = [];
+      activeScreen = 'questions-screen';
+    });
+  }
+
+  @override
+  Widget build(context) {
+    Widget screenWidget = StartScreen(switchScreen);
+
+    if (activeScreen == 'questions-screen') {
+      screenWidget = QuestionsScreen(
+        onSelectAnswer: chooseAnswer,
+      );
+    }
+
+    if (activeScreen == 'results-screen') {
+      screenWidget = ResultsScreen(
+        chosenAnswers: _selectedAnswers,
+        onRestart: restartQuiz,
+        );
+    }
+    // var screenWidget = activeScreen == 'start-screen'
+    //           ? StartScreen(switchScreen)
+    //           : const QuestionsScreen();
+
+    return MaterialApp(
+      home: Scaffold(
+        body: Container(
+          decoration: const BoxDecoration(
+            gradient: LinearGradient(colors: [
+              Color.fromARGB(255, 121, 47, 248),
+              Color.fromARGB(255, 75, 3, 199)
+            ], begin: Alignment.topLeft, end: Alignment.bottomRight),
+          ),
+          // child: activeScreen,
+          // child: activeScreen == 'start-screen'
+          //     ? StartScreen(switchScreen)
+          //     : const QuestionsScreen(),
+          child: screenWidget,
+        ),
+      ),
+    );
+  }
+}
