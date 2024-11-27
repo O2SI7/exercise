b3a1ce7a1fdba6734ce79b97f25851daaede3c2f
O2SI <O2SI_@naver.com>
2024-11-27 15:25:41 +0900

    숫자값 외 다른 입력시 오류발생

--- dart_console_app/basic/05.5_num_guess.dart
+++ dart_console_app/basic/05.5_num_guess.dart
@@ -4,26 +4,12 @@ import 'dart:math';
 import '04_unit_convertor.dart';
 
 int comRandom = Random().nextInt(100) + 1; //컴퓨터 랜덤숫자
-int turn = 0; //게임 횟수
-int maxGame = 10;
+int turn = 0; //게임 진행 횟수
+int maxGame = 10; // 도전 기회
 int inputUser = 0; // 유저입력숫자
 Future<void> main(List<String> arguments) async {
- 
-    
-    printWelcomeMessage();
-    game();
-
-   
-    // print('게임을 다시 시작하려면 Y를 입력하세요.');
-    // print('게임을 종료하려면 N을 입력하세요.');
-    // final inputchoice = stdin.readLineSync();
-    //   if (inputchoice == 'Y') {
-    //     return game();
-    //   } else if (inputchoice == 'N') {
-    //     print('ㅂ2ㅂ2');
-    //     break;
-    //   }
-  
+  printWelcomeMessage();
+  game();
 }
 
 void printWelcomeMessage() {
@@ -43,25 +29,20 @@ void game() {
     if (turn == maxGame) {
       print(' [☠️ 게임 오버 ☠️]');
       print('기회를 모두 소진했습니다. 😔');
-      print('정답은 $comRandom이었습니다.');
+      print('정답은 $comRandom이었습니다.\n');
+      reset();
+
       break;
     }
     print('숫자를 입력하세요:');
     final inputUser = int.parse(stdin.readLineSync()!);
+    if (inputUser == null) {
+      print('숫자만 입력 할 수 있습니다. 다시 적어주세요.');
+    }
     if (comRandom == inputUser) {
       print('[🎉 정답입니다! 🎉]');
       print('축하합니다! 정답은 $comRandom입니다! 🎯');
-
-      print('게임을 다시 시작하려면 Y를 입력하세요.');
-      print('게임을 종료하려면 N을 입력하세요.');
-      final inputchoice = stdin.readLineSync();
-      if (inputchoice == 'Y') {
-        print('[🔄 게임 초기화 🔄]');
-        print('1부터 100 사이의 무작위 숫자를 맞혀보세요! 😊');
-        return game();
-      } else if (inputchoice == 'N') {
-        break;
-      }
+      reset();
 
       break;
     } else if (comRandom > inputUser) {
@@ -72,6 +53,22 @@ void game() {
       print('[❌ 오답입니다!]');
       print('힌트 : 너무 높습니다! 📉');
       print('\n\n다시 시도하세요!');
+    }
+  }
+}
+
+void reset() {
+  while (true) {
+    print('게임을 다시 시작하려면 Y를 입력하세요.');
+    print('게임을 종료하려면 N을 입력하세요.');
+    final inputchoice = stdin.readLineSync();
+    if (inputchoice == 'Y') {
+      print('[🔄 게임 초기화 🔄]');
+      print('새로운 숫자를 생성합니다. 다시 시작합니다! 🎲\n');
+      print('1부터 100 사이의 무작위 숫자를 맞혀보세요! 😊');
+      return game();
+    } else if (inputchoice == 'N') {
+      break;
     }
   }
 }
