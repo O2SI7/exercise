b5ad3ada66b1a4f362112bc76c53ca45b4eddba2
O2SI <O2SI_@naver.com>
2024-11-28 11:46:24 +0900

    저장법 해결중...

--- dart_console_app/basic/06_student_manager.dart
+++ dart_console_app/basic/06_student_manager.dart
@@ -4,6 +4,7 @@ import '04_unit_convertor.dart';
 
 String? name, age, color, animal;
 List<Map<String, dynamic>> data = [];
+final Map<String, dynamic> user = {};
 
 Future<void> main(List<String> arguments) async {
   print('[✨ 간단한 데이터 관리 프로그램 ✨]');
@@ -17,13 +18,7 @@ Future<void> main(List<String> arguments) async {
     print('4. 전체 데이터 보기');
     print('5. 프로그램 종료');
 
-    final command = getUserInput([
-      '1',
-      '2',
-      '3',
-      '4',
-      '5',
-    ]);
+    final command = getUserInput(['1', '2', '3', '4', '5']);
     switch (command) {
       case '1':
         dataAdd(data);
@@ -53,21 +48,65 @@ void dataAdd(data) {
   print('[📌 데이터 추가 📌]');
   print('추가하고 싶은 데이터를 입력하세요');
   print('| 이름 | 나이 | 좋아하는 색깔 | 좋아하는 동물 |');
-  String? input = stdin.readLineSync();
+  String? name = stdin.readLineSync();
+  String? age = stdin.readLineSync();
+  String? color = stdin.readLineSync();
+  String? animal = stdin.readLineSync();
 
+  Map<String, String> a = {
+    '이름': name!,
+    '나이': age!,
+    '좋아하는 색': color!,
+    '좋아하는 동물': animal!,
+  };
 
+  data.add('');
+
+  print('잠시만요... 데이터를 추가 중입니다...✨\n');
+  print('[✅ 데이터 추가 완료 ✅]');
+  print('현재 데이터 목록');
+  print('$a');
+  print('더 추가하시겠어요? (Y/N): Y');
+  print('추가하고 싶은 데이터를 입력하세요.');
+  //String? name = stdin.readLineSync(); 추가문
+  print('잠시만요... 데이터를 추가 중입니다...✨\n');
+  print('[✅ 데이터 추가 완료 ✅]');
+  print('현재 데이터 목록');
+  //print('현재 데이터 목록'); 현재까지 추가한 목록 나와야함.
+  print('더 추가하시겠어요? (Y/N)');
+  //String? name = stdin.readLineSync(); 추가문
 
   //스페이스바 입력시 나가짐 수정
-  if (input == null || input.isEmpty) {
-    print('다시 입력해주세요\n');
-    return ;
-  }
+  // if (input == null || input.isEmpty) {
+  //   print('다시 입력해주세요\n');
+  //   return;
+  // }
 }
 
-void dataSearch() {}
+void dataSearch() {
+  print('[🔍 데이터 검색 🔍]');
+  print('찾고 싶은 데이터를 입력하세요 (예시: 이름): 김똑똑');
+//String? input = stdin.readLineSync();
+  print('데이터를 검색 중입니다... 🧐\n');
+  print('[🎯 검색 결과 🎯]');
+//print('');결과값 보여줘야함
+  print('데이터가 맞나요? 🎉');
+}
 
-void datadelete() {}
+void datadelete() {
+  print('[🗑️ 데이터 삭제 🗑️]');
+  print('삭제하고 싶은 데이터를 입력하세요 (예시: 이름): 이슬기');
+//String? input = stdin.readLineSync();
+  print('데이터를 삭제 중입니다... 💥');
+  print('[✅ 데이터 삭제 완료 ✅]');
+  print('현재 데이터 목록:');
+//print(''); 결과값 보여줘야함
+  print('삭제가 완료되었습니다! 👏');
+}
 
-void dataFullSearch() {}
-
-void off() {}
+void dataFullSearch() {
+  print('[📂 전체 데이터 보기 📂]');
+  print('현재 저장된 데이터 목록입니다:');
+  //print(''); 결과값 보여줘야함
+  print('데이터가 정리되어 있어요! ✨');
+}
